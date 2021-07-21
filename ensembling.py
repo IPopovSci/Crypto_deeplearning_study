Fixed mean ensembling, added scaling for on, disable plot_stock in run_functions.py

ensembling.py
@@ -5,15 +5,15 @@ from keras.callbacks import ModelCheckpoint
 from callbacks import mcp, custom_loss
 from keras.models import Sequential, load_model
 from attention import Attention
-from data_scaling import unscale_data
+from data_scaling import unscale_data,unscale_data_np
 from plotting import plot_results
 import numpy as np
 import os
 import tensorflow as tf
 import random
 
-ticker = 'MSFT'
-x_t, y_t, x_val, y_val, x_test_t, y_test_t = data_prep('MSFT')
+ticker = 'GME'
+x_t, y_t, x_val, y_val, x_test_t, y_test_t = data_prep(ticker)
 BATCH_SIZE = args['batch_size']
 epoch = None
 val_loss = None
@@ -35,17 +35,28 @@ def train_models(x_t, y_t, x_val, y_val, num_models=10, model_name='Default'):
 
 # train_models(x_t,y_t,x_val,y_val,5,'LSTM_MSFT')
 
-def simple_mean_ensemble(ticker, model_name='Default'):
+def simple_mean_ensemble(ticker, model_name='Default',update=True):
     preds = []
+    x_t, y_t, x_val, y_val, x_test_t, y_test_t = data_prep(ticker)
 
     for model in os.listdir(f'data\output\models\{model_name}'):
         saved_model = load_model(os.path.join(f'data\output\models\{model_name}', model),
                                  custom_objects={'custom_loss': custom_loss, 'attention': Attention})
+        if update == True:
+            saved_model.reset_states()
+            history_lstm = saved_model.fit(x_t, y_t, epochs=args["epochs"], verbose=1, batch_size=BATCH_SIZE,
+                                           shuffle=False, validation_data=(trim_dataset(x_val, BATCH_SIZE),
+                                                                           trim_dataset(y_val, BATCH_SIZE)))
         y_pred_lstm = saved_model.predict(trim_dataset(x_test_t, BATCH_SIZE), batch_size=BATCH_SIZE)
         y_pred_lstm = y_pred_lstm.flatten()
         y_pred, y_test = unscale_data(ticker, y_pred_lstm, y_test_t)
         preds.append(y_pred)
 
+    mean_preds = np.mean(preds,axis=0)
+    print(mean_preds)
+
+    y_test = trim_dataset(y_test, BATCH_SIZE)
+    plot_results(mean_preds,y_test)
 
 def update_models(ticker_list=['AMD', 'AXR', 'TSLA','DIS','GME','^GSPC','AAPL'], model_name_load='Default',
                   model_name_save='Default'):
@@ -69,5 +80,5 @@ def update_models(ticker_list=['AMD', 'AXR', 'TSLA','DIS','GME','^GSPC','AAPL'],
                                            callbacks=[mcp])
             i+=1
 
-simple_mean_ensemble('MSFT',model_name='LSTM_MSFT')
+simple_mean_ensemble(ticker,model_name='the_best',update=True)
 # update_models(model_name_load='LSTM_MSFT', model_name_save='Multi_Update_Models')