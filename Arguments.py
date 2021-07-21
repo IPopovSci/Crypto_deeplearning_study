414992966348396178e2e780021ae152fd17c95b
IvanPopAudio <72165662+IvanPopAudio@users.noreply.github.com>
2021-07-21 16:45:56 -0400

    Fix Arguments - Ivans problems with git

--- Arguments.py
+++ Arguments.py
@@ -1,16 +1,16 @@
 class Arguments:
     def __init__(self):
         self.args = {
-            'ticker': ['GME'],
+            'ticker': '^IXIC',
             'data_types': ['training', 'validation', 'test'],
             'n_components': None,  # n_components is responsible for designating number of features the data will be reduced to
-            'time_steps': 1,
+            'time_steps': 14,
             'tokens': [],
             'largest_index': None,
             'training_constant': 0.6,
             'validation_constant': 0.8,
             'batch_size': 32,
-            'epochs': 30,
+            'epochs': 10,
             'mm_path': None,
             'sc_path': None,
             'output_path': 'data\output',
