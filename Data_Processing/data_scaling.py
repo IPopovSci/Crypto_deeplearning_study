db82cac71da97ba1eca2d904c28dd4f2db10f6bb
IvanPopAudio <72165662+IvanPopAudio@users.noreply.github.com>
2022-01-25 15:28:22 -0500

    Standard scaler x and y split implement

--- Data_Processing/data_scaling.py
+++ Data_Processing/data_scaling.py
@@ -12,27 +12,42 @@ The target columns are the first 5
 returns transformed train,validation,test sets as well as the scaler'''
 def SS_transform(x_train,x_validation,x_test,y_train,y_validation,y_test,initial_training=True,SS_path=[]):
     if initial_training == False:
-        scaler = joblib.load(SS_path)
-        sc = scaler
+        ss_x = joblib.load(SS_path + ".x" )
+        ss_y = joblib.load(SS_path + ".y")
+
+        x_train_ss = ss_x.transform(x_train)
+
+        x_validation_ss = ss_x.transform(x_validation)
+
+        x_test_ss = ss_x.transform(x_test)
+
+        y_train_ss = ss_y.fit_transform(y_train)
+
+        y_validation_ss = ss_y.fit_transform(y_validation)
+
+        y_test_ss = ss_y.fit_transform(y_test)
+
     elif initial_training == True:
-        sc = StandardScaler()
+        sc_x = StandardScaler()
+        sc_y = StandardScaler()
 
-    x_train_ss = sc.fit_transform(x_train)
+        x_train_ss = sc_x.fit_transform(x_train)
 
-    x_validation_ss = sc.transform(x_validation)
+        x_validation_ss = sc_x.transform(x_validation)
 
-    x_test_ss = sc.transform(x_test)
+        x_test_ss = sc_x.transform(x_test)
 
-    y_train_ss = sc.fit_transform(y_train)
+        y_train_ss = sc_y.fit_transform(y_train)
 
-    y_validation_ss = sc.fit_transform(y_validation)
+        y_validation_ss = sc_y.fit_transform(y_validation)
 
-    y_test_ss = sc.fit_transform(y_test)
+        y_test_ss = sc_y.fit_transform(y_test)
 
     if initial_training == True:
-        joblib.dump(sc, SS_path)
+        joblib.dump(sc_x, SS_path + '.x')
+        joblib.dump(sc_y, SS_path + '.y')
 
-    return x_train_ss,x_validation_ss,x_test_ss,y_train_ss,y_validation_ss,y_test_ss,sc
+    return x_train_ss,x_validation_ss,x_test_ss,y_train_ss,y_validation_ss,y_test_ss,sc_y
 '''For transfer learning on small datasets, no validation, no test'''
 def SS_transform_small(x_train):
 
@@ -87,7 +102,7 @@ def min_max_transform(x_train,x_validation,x_test,y_train, y_validation, y_test,
         joblib.dump(mm_x, MM_path + '.x')
         joblib.dump(mm_y, MM_path + '.y')
 
-    return x_train,x_validation,x_test,y_train, y_validation, y_test, mm
+    return x_train,x_validation,x_test,y_train, y_validation, y_test, mm_y
 
 '''min max for a singular dataset'''
 
