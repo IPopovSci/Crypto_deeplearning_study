ef7d8913c51f2db9779dffc06e81ae38597ea220
IvanPopAudio <72165662+IvanPopAudio@users.noreply.github.com>
2022-03-03 12:34:50 -0500

    Clean up and more comments

--- Data_Processing/Detrending.py
+++ Data_Processing/Detrending.py
@@ -26,43 +26,28 @@ def test_stationarity(timeseries):
 
 '''Creates a new dataset filled with the differences of values of each day
 This will make the data stationary (Use the above test to check), as well
-as easier to work with for neural networks'''
+as easier to work with for neural networks
+Inputs: dataset (Pandas), ta (True/False)
+Outputs: Pandas Dataframe'''
 
 
-def row_difference(df,ta):
-    df_data = df.iloc[:,:5]
+def row_difference(df, ta):
+    df_data = df.iloc[:, :5]
 
-
-
-
-
-    df_ta = df.iloc[:,5:]
-
-    #df_ta = np.log1p(df_ta) #take log of ta? Does that makes sense? To avoid any stat imbalance
-    '''Implementing categorization on data'''
-    #df_data_lg = np.log1p(df_data)  # take a log, to smooth out any shmuckery
-
-    #df_ta = np.log1p(df_ta)
-
+    df_ta = df.iloc[:, 5:]
 
     df_diff_data = df_data.pct_change(axis=0)
 
-    print(df_diff_data[-30:])
-
-    #df_diff_ta = df_ta.pct_change(axis=0)
-    #df_diff_ta = df_diff_ta.fillna(0)
     if ta == True:
-        df_diff = pd.concat([df_diff_data,df_ta],axis=1)
+        df_diff = pd.concat([df_diff_data, df_ta], axis=1)
     else:
         df_diff = pd.concat([df_diff_data], axis=1)
 
-
-
-    df_diff = df_diff.iloc[1:, :] #this drops the first row (For avoiding N/A)
+    df_diff = df_diff.iloc[1:, :]  # this drops the first row (For avoiding N/A)
     '''Debug options'''
-    #pd.set_option('max_columns', None)
+    # pd.set_option('max_columns', None)
 
-    #print(df_diff.head(n=30))
+    # print(df_diff.head(n=30))
 
     return df_diff
 
